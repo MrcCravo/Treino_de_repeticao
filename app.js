c6bf84fde6429f3e544118f10044a8cd8e2eab89
MrcCravo <ferramentas@maracs.com.br>
2023-10-05 21:20:36 -0300

    Treino por repetição

---
+++ app.js
@@ -0,0 +1,31 @@
+// essa é uma revisão do exercício executado anteriormente
+// a ideia aqui não é realizar uma aplicação funcional, é apenas um atividade de repetição
+
+
+
+alert("Treinando a tag prompt");
+
+let nome = prompt("Qual seu nome");
+let corPreferida = prompt("qual sua cor preferida");
+let idade = prompt("qual a sua idade");
+
+console.log (`${nome} ${corPreferida} ${idade}`);
+console.log (nome);
+
+if (idade >= 18) {
+    console.log("Você é maior de idade");
+    console.log("agora você pode tirar sua carteira de motorista");
+    console.log("agora você pode beber responsavelmente");
+}   else {
+        console.log("Você ainda é menor de idade");
+        console.log("Você não pode dirigir");
+        console.log("Você não pode se divertir bebendo");
+}
+
+
+
+
+
+
+
+
